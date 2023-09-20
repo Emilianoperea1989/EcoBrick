51b4da038a92edd1496828b66cd2964030a518f8
Emiliano Perea <emilianoperea10@gmail.com>
2023-09-20 10:45:58 -0300

    maquetando especificaciones

--- src/components/Especificaciones.tsx
+++ src/components/Especificaciones.tsx
@@ -7,7 +7,7 @@ export const Especificaciones = () => {
         <div className="card-container">
           <div className="card-img">
           <h2 className="card-title">Ladrillos ecologicos premoldeados</h2>
-            <img src="./assets/infografia.png" alt="infografia" />
+            <img src="https://res.cloudinary.com/duqilis2i/image/upload/v1695174127/C%C3%ADrculo_completo_con_%C3%ADconos_ecoladirllos._PNG_wgcsyo.png" alt="infografia" />
           </div>
         </div>
       </section>
